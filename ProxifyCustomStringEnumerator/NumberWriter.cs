3fc28b1985b33dff5776cd45e5f2812a2be7208d
Ben <danquahwhite@gmail.com>
2022-11-12 07:25:27 +0000

    push

---
+++ ProxifyCustomStringEnumerator/NumberWriter.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomStringEnumerator.Tests;
+
+public static class NumberWriter
+{
+    public static void Display(int x)
+    {
+        Console.WriteLine("Integer: " + x);
+    }
+    public static void Display(long x)
+    {
+        Console.WriteLine("Long: " + x);
+    }
+}
--- ProxifyCustomStringEnumerator/Program.cs
+++ ProxifyCustomStringEnumerator/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+using CustomStringEnumerator.Tests;
+
+Queue<string> queue = new();
+
 var collection = new string[] {  };
 var config = new EnumeratorConfig
 {
@@ -14,4 +19,6 @@ foreach (var s in enumerator)
     Console.WriteLine(s);
 }
 
+var x = 10;
+NumberWriter.Display(x);
 Console.WriteLine("Hello, World!");
